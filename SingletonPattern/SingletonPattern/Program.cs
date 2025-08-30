0efd2e684a5a702f6b0753a79b82661b38d7cbc9
Valentina <valentinaolmosoo@gmail.com>
2025-08-30 17:04:35 -0300

    Primera parte singleton terminado

---
+++ SingletonPattern/SingletonPattern/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using SingletonPattern.Singleton;
+
+namespace SingletonPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Logger.instancia;
+            a.Info("Primer mensaje");
+            a.Info("Segundo mensaje");
+            a.Info("Tercer mensaje");
+
+            var b = Logger.instancia;
+            b.Dump();
+
+            Console.WriteLine($"¿a y b referencian el mismo obj?: {ReferenceEquals(a, b)}");
+
+
+
+        }
+    }
+}
---
+++ SingletonPattern/SingletonPattern/Singleton/Logger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern.Singleton
+{
+    public sealed class Logger
+    {
+        private static readonly Lazy<Logger> inst = new Lazy<Logger>(() => new Logger());
+        public static Logger instancia => inst.Value;
+        private readonly List<string> historial = new List<string>();
+
+        private Logger()
+        {
+
+        }
+        
+        public void Info(string mensaje)
+        {
+            string msg = $"[INFO]: {mensaje}";
+            historial.Add(msg);
+            Console.WriteLine(msg);
+        }
+
+        public void Dump()
+        {
+            Console.WriteLine("-- HISTORIAL DE MENSAJES --");
+            foreach (var i in historial)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+}
